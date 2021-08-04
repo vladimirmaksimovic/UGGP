4bcd0cbe3d182b45c995e9b221723a8b9dcd1e33
vladimirmaksimovic <vmaksimovic.cr34@gmail.com>
2021-08-05 01:11:55 +0200

    styling footer copyright section

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,45 +4,45 @@
 
 // open hamburger navbar
 function hamburger() {
-  const li = document.getElementsByClassName("nav-item")
+  const li = document.getElementsByClassName("nav-item");
 
   for (let i = 0; i < li.length; i++) {
     if (li[i].className === "nav-item") {
-      li[i].className += " responsive"
+      li[i].className += " responsive";
     } else {
-      li[i].className = "nav-item"
+      li[i].className = "nav-item";
     }
   }
 }
 
 // close hamburger navbar
-const a = document.getElementsByClassName("nav-link")
+const a = document.getElementsByClassName("nav-link");
 
 for (let i = 0; i < a.length; i++) {
   a[i].addEventListener("click", function () {
-    const li = document.getElementsByClassName("nav-item")
+    const li = document.getElementsByClassName("nav-item");
     for (let i = 0; i < li.length; i++) {
-      li[i].className = "nav-item"
+      li[i].className = "nav-item";
     }
-  })
+  });
 }
 
 /**
  * NEWS COLLAPSE
  */
 
-const newsArticle = document.getElementsByClassName("news-article")
+const newsArticle = document.getElementsByClassName("news-article");
 
 for (let i = 0; i < newsArticle.length; i++) {
   newsArticle[i].addEventListener("click", function () {
-    this.classList.toggle("active")
-    const newsContent = this.lastElementChild
+    this.classList.toggle("active");
+    const newsContent = this.lastElementChild;
     if (newsContent.style.maxHeight) {
-      newsContent.style.maxHeight = null
+      newsContent.style.maxHeight = null;
     } else {
-      newsContent.style.maxHeight = newsContent.scrollHeight + "px"
+      newsContent.style.maxHeight = newsContent.scrollHeight + "px";
     }
-  })
+  });
 }
 
 /**
@@ -50,27 +50,48 @@ for (let i = 0; i < newsArticle.length; i++) {
  */
 
 // get modal elements
-const modal = document.getElementsByClassName("modal-body")
+const modal = document.getElementsByClassName("modal-body");
 // get img elements
-const img = document.getElementsByClassName("img-item")
+const img = document.getElementsByClassName("img-item");
 // get close btn`s
-const closeModalBtn = document.getElementsByClassName("close-modal")
+const closeModalBtn = document.getElementsByClassName("close-modal");
 
 // go through images
 for (let i = 0; i < img.length; i++) {
   // open modal event
-  img[i].addEventListener("click", openModal)
+  img[i].addEventListener("click", openModal);
 
   // open modal f-n
   function openModal() {
-    modal[i].style.display = "flex"
+    modal[i].style.display = "flex";
   }
 
   // close modal event
-  closeModalBtn[i].addEventListener("click", closeModal)
+  closeModalBtn[i].addEventListener("click", closeModal);
 
   // close modal f-n
   function closeModal() {
-    modal[i].style.display = "none"
+    modal[i].style.display = "none";
   }
+}
+
+/**
+ * SHOW DESIGNER LINK
+ */
+
+const designer = document.querySelector(".designer");
+const alterEgo = document.querySelector(".alt-ego");
+const designIcon = document.querySelector(".fa-paint-roller");
+
+designer.addEventListener("mouseenter", showAltEgo);
+designer.addEventListener("mouseleave", hideAltEgo);
+
+function showAltEgo() {
+  alterEgo.style.display = "inline";
+  designIcon.style.color = "#ffc400";
+}
+
+function hideAltEgo() {
+  alterEgo.style.display = "none";
+  designIcon.style.color = "#ccc";
 }
